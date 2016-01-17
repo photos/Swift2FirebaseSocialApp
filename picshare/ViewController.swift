7bd3cb2e35a4f3d83851d318084f5e8e9300b473
photos <myjellymail@gmail.com>
2016-01-17 12:07:06 -0800

    Initial Commit

---
+++ picshare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  picshare
+//
+//  Created by Forrest Collins on 1/17/16.
+//  Copyright © 2016 helloTouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
